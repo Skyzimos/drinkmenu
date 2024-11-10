170af4aded231d245d3f1e4d6a1f1e75fab3587e
Skyzimos <95323257+Skyzimos@users.noreply.github.com>
2024-11-10 17:20:35 -0600

    Updated data.js to latest framework

--- scripts/data.js
+++ scripts/data.js
@@ -1,250 +1,252 @@
-window.__shared_data = {
-    _production: false,
-    _list_data: {
-        ['Beers']: {
-            subtitle: "It's 5 o'clock somewhere!",
-            clickable: true,
-            drinks: [
-                'Heineken',
-                'Coors Light',
-                'Corona Extra',
-                'Coors Banquet',
-                'Miller Lite',
-                'Michelob Golden Light',
-                'Miller Genuine Draft',
-                'Michelob Ultra',
-                'Guiness Extra Stout',
-                'Modelo',
-                'Bud Light',
-                'Budweiser',
-                'Miller High Life',
-                'Stella Artois',
-                'Bud Light Lime',
-            ]
-        },
-        ['Tap Beers']: {
-            subtitle: '-',
-            clickable: false,
-            drinks: [
-                'Coors Light',
-                'Miller Lite',
-                'Fresh Squeezed IPA',
-                'Castle Danger Cream Ale',
-                'Downeast Strawberry',
-                'Blue Moon',
-                'Warpigs Foggy Geezer',
-                'Michelob Golden Light',
-                'Voodoo Ranger 1985 Mango',
-                'Sam Adams Octoberfest',
-                'Surly Furious',
-                'Shiner Bock',
-                'Summit EPA',
-                'Summer Shandy',
-                'Liftbridge Mango Blonde',
-                'Lupulin Fashion Mullet IPA',
-                'Loon Juice',
-                'Utepils Skölsch',
-                'Kona Big Wave',
-            ]
-        },
-        ['Seltzers']: {
-            subtitle: '-',
-            clickable: true,
-            drinks: [
-                'White Claw (Black Cherry)',
-                'White Claw (Raspberry)',
-                'White Claw (Peach)',
-                'Carbliss (Lemon-Lime)',
-                'Carbliss (Black Raspberry)',
-            ]
-        },
-        ['Shots']: {
-            subtitle: 'SHOTS! SHOTS! SHOTS!',
-            clickable: true,
-            drinks: [
-                'Lemon Drop',
-                'Grape Ape',
-                'Bazooka Joe',
-                'Apple Sauce Cocktail',
-                'Salty Dog',
-                'Green Tea',
-                'Kamakazi',
-                'Buttery Nipple',
-                'Washington Apple',
-                'Salted Nut Roll',
-                'Kool Aid',
-                'Melon Ball',
-                'Peanut Butter and Jelly',
-                'Jolly Rancher',
-                'Red Headed Slut',
-                'Tang',
-                'Surfer on Acid',
-                'Scooby Snack',
-                'Bong Water',
-                'F***ing Awesome',
-                'Liquid Marijuana',
-                'Southern Hospitality',
-                'Orange Peel',
-                'Jack Apple Sour',
-            ]
-        },
-        ['Cocktails']: {
-            subtitle: 'Liquid Magic. Literally.',
-            clickable: true,
-            drinks: [
-                'Long Island Tea',
-                'Blue MF',
-                'Tokyo Tea',
-                'Bahama Mama',
-                'Jackup Punch', // need pic
-                'Mai Tai', // need pic
-                'Sex on the Beach',
-                'Colorado Bulldog', // need pic
-                'Fruit Roll Up', // on phone
-                'Lime Margarita', // need pic
-                'Blue Hawaiian',
-                "Courtney's Coffee", // need pic
-                'Old Fashioned', // need pic
-                'Manhattan', // need pic
-                'White Russian',
-                'Greyhound',
-                'Tequila Sunrise', // on phone
-                'Iron Butterfly', // need pic
-                'Black Russian', // need pic (make first, then make iron butterfly)
-                'Salty Dog',
-            ]
-        },
-        ['Martinis']: {
-            subtitle: 'Shaken, not stirred.',
-            clickable: true,
-            drinks: [
-                'Apple-tini',
-                'Cosmopolitan',
-                'Dirty Martini',
-                'Choco-tini',
-                'Lemon-drop Martini',
-                'Strawberry Lemon-drop Martini'
-            ]
-        },
-        ['Wines']: {
-            subtitle: 'Words so fancy you probably can\'t even say them.',
-            clickable: true,
-            drinks: [
-                '__Red Wines',
-                'Merlot (Barone Fini)',
-                'Red Blend (Ménage à Trois)',   
-                'Cabernet Sauvignon‎ ‎ ‎ ‎(Sycamore Lane)',
-    
-                '__White Wines',
-                'Prosecco (Zonin)',
-                'White Zinfandel (Buehler)',
-                'Chardonnay (Hess)',
-                'Sauvignon Blanc (Sutter Home)',
-            ]
-        },
-        ['Liquors']: {
-            subtitle: `We just might have what you want.`,
-            clickable: false,
-            drinks: [
-                '__Vodkas',
-                'Philips',
-                'Tito\'s',
-                'Stoli',
-                'Svedka',
-                'Smirnoff',
-                'Grey Goose',
-                'Ketel One',
-                'Vanilla Smirnoff',
-                'Raspberry Smirnoff',
-                'Citrus Smirnoff',
-                'Blue Raspberry Stoli',
-                'Peppar',
-                'Ciroc',
-                
-                '__Gin',
-                'Philips',
-                'Tanqueray',
-                'Sapphire',
-                'Hendrick\'s',
-    
-                '__Tequila',
-                'Philips',
-                'Jose Cuervo',
-                '1800',
-                'Don Julio Blanco',
-                'Patron Blanco',
-                'Casamigos Blanco',
-                'Casamigos Reposado',
-    
-                '__Rum',
-                'Philips',
-                'Captain Morgan',
-                'Bacardi',
-                'Bacardi Limon',
-                'Bacardi Razz',
-                'Cruzan Coconut',
-                'Myers\'s Dark',
-    
-                '__Whiskey',
-                'Philips',
-                'Jameson',
-                'Jack Daniel\'s',
-                'Jack Fire',
-                'Seagram\'s 7',
-                'Skrewball',
-                'Jim Beam',
-                'Windsor',
-                'Southern Comfort',
-                'Crown',
-                'Crown Apple',
-                'Yukon Jack',
-                'Maker\'s Mark',
-                'Seagram\'s VO',
-                'Canadian Club',
-                'Wild Turkey',
-    
-                '__Bourbon',
-                'Bulleit',
-                'Bulleit Rye',
-    
-                '__Cognac',
-                'Hennessy',
-    
-                '__Brandy',
-                'Philips',
-                'Christian Brothers',
-                'E&J',
-    
-                '__Scotch',
-                'Johnnie Walker Black',
-                'Johnnie Walker Red',
-                'Glenlivet',
+const event = new CustomEvent('__share_data', {
+    detail: { 
+        _production: true,
+        _shared_data: {
+            ['Beers']: {
+                subtitle: "It's 5 o'clock somewhere!",
+                clickable: true,
+                drinks: [
+                    'Heineken',
+                    'Coors Light',
+                    'Corona Extra',
+                    'Coors Banquet',
+                    'Miller Lite',
+                    'Michelob Golden Light',
+                    'Miller Genuine Draft',
+                    'Michelob Ultra',
+                    'Guiness Extra Stout',
+                    'Modelo',
+                    'Bud Light',
+                    'Budweiser',
+                    'Miller High Life',
+                    'Stella Artois',
+                    'Bud Light Lime',
+                ]
+            },
+            ['Tap Beers']: {
+                subtitle: '-',
+                clickable: false,
+                drinks: [
+                    'Coors Light',
+                    'Miller Lite',
+                    'Fresh Squeezed IPA',
+                    'Castle Danger Cream Ale',
+                    'Downeast Strawberry',
+                    'Blue Moon',
+                    'Warpigs Foggy Geezer',
+                    'Michelob Golden Light',
+                    'Voodoo Ranger 1985 Mango',
+                    'Sam Adams Octoberfest',
+                    'Surly Furious',
+                    'Shiner Bock',
+                    'Summit EPA',
+                    'Summer Shandy',
+                    'Liftbridge Mango Blonde',
+                    'Lupulin Fashion Mullet IPA',
+                    'Loon Juice',
+                    'Utepils Skölsch',
+                    'Kona Big Wave',
+                ]
+            },
+            ['Seltzers']: {
+                subtitle: '-',
+                clickable: true,
+                drinks: [
+                    'White Claw (Black Cherry)',
+                    'White Claw (Raspberry)',
+                    'White Claw (Peach)',
+                    'Carbliss (Lemon-Lime)',
+                    'Carbliss (Black Raspberry)',
+                ]
+            },
+            ['Shots']: {
+                subtitle: 'SHOTS! SHOTS! SHOTS!',
+                clickable: true,
+                drinks: [
+                    'Lemon Drop',
+                    'Grape Ape',
+                    'Bazooka Joe',
+                    'Apple Sauce Cocktail',
+                    'Salty Dog',
+                    'Green Tea',
+                    'Kamakazi',
+                    'Buttery Nipple',
+                    'Washington Apple',
+                    'Salted Nut Roll',
+                    'Kool Aid',
+                    'Melon Ball',
+                    'Peanut Butter and Jelly',
+                    'Jolly Rancher',
+                    'Red Headed Slut',
+                    'Tang',
+                    'Surfer on Acid',
+                    'Scooby Snack',
+                    'Bong Water',
+                    'F***ing Awesome',
+                    'Liquid Marijuana',
+                    'Southern Hospitality',
+                    'Orange Peel',
+                    'Jack Apple Sour',
+                ]
+            },
+            ['Cocktails']: {
+                subtitle: 'Liquid Magic. Literally.',
+                clickable: true,
+                drinks: [
+                    'Long Island Tea',
+                    'Blue MF',
+                    'Tokyo Tea',
+                    'Bahama Mama',
+                    'Jackup Punch', // need pic
+                    'Mai Tai', // need pic
+                    'Sex on the Beach',
+                    'Colorado Bulldog', // need pic
+                    'Fruit Roll Up', // on phone
+                    'Lime Margarita', // need pic
+                    'Blue Hawaiian',
+                    "Courtney's Coffee", // need pic
+                    'Old Fashioned', // need pic
+                    'Manhattan', // need pic
+                    'White Russian',
+                    'Greyhound',
+                    'Tequila Sunrise', // on phone
+                    'Iron Butterfly', // need pic
+                    'Black Russian', // need pic (make first, then make iron butterfly)
+                    'Salty Dog',
+                ]
+            },
+            ['Martinis']: {
+                subtitle: 'Shaken, not stirred.',
+                clickable: true,
+                drinks: [
+                    'Apple-tini',
+                    'Cosmopolitan',
+                    'Dirty Martini',
+                    'Choco-tini',
+                    'Lemon-drop Martini',
+                    'Strawberry Lemon-drop Martini'
+                ]
+            },
+            ['Wines']: {
+                subtitle: 'Words so fancy you probably can\'t even say them.',
+                clickable: true,
+                drinks: [
+                    '__Red Wines',
+                    'Merlot (Barone Fini)',
+                    'Red Blend (Ménage à Trois)',
+                    'Cabernet Sauvignon‎ ‎ ‎ ‎(Sycamore Lane)',
+        
+                    '__White Wines',
+                    'Prosecco (Zonin)',
+                    'White Zinfandel (Buehler)',
+                    'Chardonnay (Hess)',
+                    'Sauvignon Blanc (Sutter Home)',
+                ]
+            },
+            ['Liquors']: {
+                subtitle: `We just might have what you want.`,
+                clickable: false,
+                drinks: [
+                    '__Vodkas',
+                    'Philips',
+                    'Tito\'s',
+                    'Stoli',
+                    'Svedka',
+                    'Smirnoff',
+                    'Grey Goose',
+                    'Ketel One',
+                    'Vanilla Smirnoff',
+                    'Raspberry Smirnoff',
+                    'Citrus Smirnoff',
+                    'Blue Raspberry Stoli',
+                    'Peppar',
+                    'Ciroc',
+                    
+                    '__Gin',
+                    'Philips',
+                    'Tanqueray',
+                    'Sapphire',
+                    'Hendrick\'s',
+        
+                    '__Tequila',
+                    'Philips',
+                    'Jose Cuervo',
+                    '1800',
+                    'Don Julio Blanco',
+                    'Patron Blanco',
+                    'Casamigos Blanco',
+                    'Casamigos Reposado',
+        
+                    '__Rum',
+                    'Philips',
+                    'Captain Morgan',
+                    'Bacardi',
+                    'Bacardi Limon',
+                    'Bacardi Razz',
+                    'Cruzan Coconut',
+                    'Myers\'s Dark',
+        
+                    '__Whiskey',
+                    'Philips',
+                    'Jameson',
+                    'Jack Daniel\'s',
+                    'Jack Fire',
+                    'Seagram\'s 7',
+                    'Skrewball',
+                    'Jim Beam',
+                    'Windsor',
+                    'Southern Comfort',
+                    'Crown',
+                    'Crown Apple',
+                    'Yukon Jack',
+                    'Maker\'s Mark',
+                    'Seagram\'s VO',
+                    'Canadian Club',
+                    'Wild Turkey',
+        
+                    '__Bourbon',
+                    'Bulleit',
+                    'Bulleit Rye',
+        
+                    '__Cognac',
+                    'Hennessy',
+        
+                    '__Brandy',
+                    'Philips',
+                    'Christian Brothers',
+                    'E&J',
+        
+                    '__Scotch',
+                    'Johnnie Walker Black',
+                    'Johnnie Walker Red',
+                    'Glenlivet',
+                    ],
+            },
+            ['Non-Alcoholic']: {
+                subtitle: '-', // all of the taste, none of the tease, for those who want to get home in one piece
+                clickable: true,
+                drinks: [
+                    '__Beers',
+                    'Heineken 0.0%',
+                    "O'doul's Amber",
+        
+                    '__Juices',
+                    'Cranberry Juice',
+                    'Grapefruit Juice',
+                    'Orange Juice',
+                    'Pineapple Juice',
+        
+                    '__Energy Drinks',
+                    'Red Bull',
+                    'Red Bull (Sugar Free)',
+        
+                    '__Other',
+                    'Pepsi Products',
+                    'Shirley Temple',
                 ],
-        },
-        ['Non-Alcoholic']: {
-            subtitle: '-', // all of the taste, none of the tease, for those who want to get home in one piece
-            clickable: true,
-            drinks: [
-                '__Beers',
-                'Heineken 0.0%',
-                "O'doul's Amber",
-    
-                '__Juices',
-                'Cranberry Juice',
-                'Grapefruit Juice',
-                'Orange Juice',
-                'Pineapple Juice',
-    
-                '__Energy Drinks',
-                'Red Bull',
-                'Red Bull (Sugar Free)',
-    
-                '__Other',
-                'Pepsi Products',
-                'Shirley Temple',
-            ],
+            }
         }
-    }
-}
+     }
+});
 
-console.log(window.__shared_data)
+document.dispatchEvent(event);
